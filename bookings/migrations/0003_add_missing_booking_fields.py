236c7f32bd60b101ee65a7bae9647480cf367c86
benjo <benjomabrasado@gmail.com>
2026-03-08 22:50:12 +0800

    Add missing migration for guest_id_document and message_to_host fields

---
+++ bookings/migrations/0003_add_missing_booking_fields.py
@@ -0,0 +1,32 @@
+"""Add missing fields to Booking model."""
+
+from django.db import migrations, models
+import django.db.models.deletion
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('bookings', '0002_add_exclusion_constraint'),
+        ('users', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='booking',
+            name='guest_id_document',
+            field=models.ForeignKey(
+                blank=True,
+                help_text='Guest ID document used for verification',
+                null=True,
+                on_delete=django.db.models.deletion.SET_NULL,
+                related_name='bookings',
+                to='users.guestid',
+            ),
+        ),
+        migrations.AddField(
+            model_name='booking',
+            name='message_to_host',
+            field=models.TextField(blank=True, help_text='Initial message from guest to host'),
+        ),
+    ]
