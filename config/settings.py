7f0cb8aca3568b69529f267b640a5610dc3ef663
benjo <benjomabrasado@gmail.com>
2026-03-10 11:11:06 +0800

    pagre size

--- config/settings.py
+++ config/settings.py
@@ -215,7 +215,7 @@ REST_FRAMEWORK = {
         'rest_framework.filters.OrderingFilter',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 20,
+    'PAGE_SIZE': 200,  # Increased to show all listings on search page
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',
     ],
